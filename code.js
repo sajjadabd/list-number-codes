c195aa2b78a92d419a9aa3e1fd379658156c9aa0
sajjad abdollahi <mynameissajjad@gmail.com>
2022-04-04 21:50:04 +0430

    set number increasing in column

--- code.js
+++ code.js
@@ -5,24 +5,47 @@ let end = 3;
 
 // get input from alert
 let month = 1;
-let day = 15;
-start = 43;
-end = 84;
+let day = 16;
+start = 1;
+end = 21*5;
+// end = 21;
+// end = 21*2;
+// end = 21*3;
 
-let content = '' ;
+
+let content = `` ;
 console.log(`start processing at ${start}`);
 
-for(let i = start; i <= end; i+=3){
-    content += `
-    <tr>
-      <td>${month}-${day}-${i}</td>
-      <td>${month}-${day}-${i+1}</td>
-      <td>${month}-${day}-${i+2}</td>
-    </tr>
-    `;
+for(let i = start; i <= end; i++){
+  if(i % 21 == 1){ 
+    content += `<div class="container">`;
+  }
+
+  if(i % 7 == 1) { 
+    content += `<div class="column">`;
+    content += `<div class="cell">${month}-${day}-${i}</div>`;
+  } else if(i % 7 == 0) {
+    content += `<div class="cell">${month}-${day}-${i}</div>`;
+    content += `</div>`;
+  } else {
+    content += `<div class="cell">${month}-${day}-${i}</div>`;
+  }
+
+  if(i % 21 == 0){
+    content += `</div>`;
+  }
 }
+
+// content += `
+// <div class="column">
+//   <div class="cell">${month}-${day}-${i}</div>
+//   <div class="cell">${month}-${day}-${i+1}</div>
+//   <div class="cell">${month}-${day}-${i+2}</div>
+// </div>
+// `;
+
 console.log(`end processing at ${end}`);
 
-//console.log(content);
+
 
 table.innerHTML = content;
